33542e800f0f0b8c26783fa7e4dd6b21b07dcc74
KazukiAmakawa <KazukiAmakawa@gmail.com>
2018-04-03 21:24:29 +0800

    c files with bug, but finished

--- CARLA.c
+++ CARLA.c
@@ -13,8 +13,13 @@
 #include <time.h>
 
 
+
 const double pi = 3.141592653589793;
 const double e = 2.718281828459045;
+double Cost(int var, double Parameter[var]);
+
+
+/*===================DO NOT CHANGE ANYTHING BELOW===================*/
 
 
 double Random(int seed){
@@ -41,36 +46,43 @@ double Random(int seed){
 }
 
 
+int compare(const void *a, const void *b){  
+   return *(double *)a - *(double *)b;   
+}  
 
 
-double Algorithm(int var, int kase, double gw, double gh, double Interval[var][2]){
+
+
+void Algorithm(int var, int ttl, double gw, double gh, double Interval[var][2]){
 /*	
 	//Function Instruction:
 	This function is main function of CARLA Method.
 
 	//Parameter Instruction:
 	int var = Total parameter will learning;
-	int kase = Total iterator loop during the learning ;
+	int ttl = Total iterator loop during the learning ;
 	double gw = Parameter gw of CARLA method;
 	double gh = Parameter gh of CARLA method;
 	Interval[var][2] = [[X1Min, X1Max], ... ,[XnMin, XnMax]] where n = var;
 
-	return Learning result;
+	return Learning result array;
 */
 
 
 	//Saving Definition
-	double x[var][kase];				//To save all decision point
-	double J[var][kase];				//To save all cost value
+	double x[var][ttl];					//To save all decision point
+	double J[var][ttl];					//To save all cost value
 	double Jmed[var];					//To save medium value of all J
 	double Jmin[var];					//To save minumum value of all J
-	double alpha[var][kase + 1];		//To save all PDF parameter
-	double beta[var][kase + 1];			//To save all reinforcement parameter
+	double alpha[var][ttl + 1];			//To save all PDF parameter
+	double beta[var][ttl + 1];			//To save all reinforcement parameter
 	double lambda[var];					//To save all lambda parameter
 	double sigma[var];					//To save all sigma parameter
 
+
 	int seed = time(NULL);				//For random number getting
 	double z;							//A random number which is the PDF integral value
+	double tem;							//A temple tank for saving calculation value
 
 
 	//Pretreatment, Calculate lambda and sigma
@@ -81,15 +93,18 @@ double Algorithm(int var, int kase, double gw, double gh, double Interval[var][2
 
 
 	//Main Loop
-	for(int ttl = 0; ttl < kase; ttl ++){
+	for(int kase = 0; kase < ttl; kase ++){
 		for(int par = 0; par < var; par ++){
-			//Get the random number z, which is the PDF integral value
+		/*
+			==========Get the random number z, which is the PDF integral value==========
+		*/
 			z = Random(seed);
 			seed = (int)(z * 100);
 
-			/*
-				Newton's Method, get x
-			*/
+
+		/*
+			========================Newton's Method, get x========================
+		*/
 
 			//Definition and Initialization
 			double delta = (Interval[par][0] + Interval[par][1]) / 2;
@@ -110,7 +125,7 @@ double Algorithm(int var, int kase, double gw, double gh, double Interval[var][2
 				for(int k = 1; k <= kase; k ++){
 					tem = exp( pow((delta - x[par][k-1]), 2) / (2 * sigma[par] * sigma[par]) );
 					tem = beta[par][k] * sigma[par] * lambda[par] * sqrt(2 * pi) / 2 * tem;
-					dFx = alpha[par][k](dFx + tem);
+					dFx = alpha[par][k] * (dFx + tem);
 				}
 
 				//Newton's iterator
@@ -118,48 +133,100 @@ double Algorithm(int var, int kase, double gw, double gh, double Interval[var][2
 				delta = delta - (Fx - z) / dFx;
 				
 				//Condition judgement
-				if (delta - Remdelta < 0.000001) && (delta - Remdelta < 0.000001):
+				if ((delta - Remdelta < 0.000001) && (delta - Remdelta > -0.000001))
 					break;
 			}
 
 			x[par][kase] = delta;
 
 
-			//Calculate the cost J
+		/*
+			=========================Calculate the cost J=========================
+		*/
+
+			//Get the parameter group at present.
 			double Parameter[var];
 			for(int loop = 0; loop < var; loop ++){
 				if (loop <= par){
-
+					Parameter[var] = x[loop][kase];
 				}
 				else{
-
+					Parameter[var] = x[loop][kase - 1];
 				}
 			}
 
-			//Reflesh the medium and minimum cost
+			//Calculate the cost
+			J[par][kase] = Cost(var, Parameter);
 
 
-			//Calculate the reinforcement value beta
+		/*
+			====================Reflesh the medium and minimum cost====================
+		*/
+			//Calculation of Jmed
+			//Definition and Initialization
+			double TemJ[kase];
+			memcpy(TemJ, J[par], sizeof(TemJ));
+			qsort(TemJ , kase, sizeof(double), compare);  
+			Jmed[par] = TemJ[(int)(kase / 2)];
 
 
-			//Calculate the PDF parameter alpha and get the PDF of next koop
+			//Calculation of Jmin
+			if (kase == 0)		
+				Jmin[par] = J[par][kase];
+			else				
+				Jmin[par] = (Jmin[par] > J[par][kase])? Jmin[par] : J[par][kase];
+
+
+		/*
+			====================Calculate the reinforcement value beta====================
+		*/
+			if ((Jmed[par] - Jmin[par]) < 0.000001 && (Jmed[par] - Jmin[par]) > -0.000001)
+				beta[par][kase + 1] = 0;
+			else{
+				tem = (Jmed[par] - J[par][kase]) / (Jmed[par] - Jmin[par]);
+				beta[par][kase + 1] = (tem > 0)? tem : 0;
+			}
+
+
+		/*
+			==========Calculate the PDF parameter alpha and get the PDF of next koop==========
+		*/
+			tem = erf( (Interval[par][1] - x[par][kase]) / (sqrt(2) * sigma[par]) ) - erf( (Interval[par][0] - x[par][kase]) / (sqrt(2) * sigma[par]) );
+			tem = beta[par][kase] * lambda[par] * sigma[par] * sqrt(2 * pi) / 2 * tem;
+			alpha[par][kase] = (double)1 / (1 + tem);
 			
+			printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t\n", z, x[par][kase], J[par][kase], Jmed[par], Jmin[par], alpha[par][kase + 1], beta[par][kase + 1]);
 		}
 	}
 
 	//Judgement, calculate the exception of PDF, and make a decision
-	double Decision = 0;
+	//freopen("After", "w", stdout);
+	for(int par = 0; par < var; par ++){
+		double Output = 0;
+		double LenIntervar = 1 / 10000 * (Interval[par][1] - Interval[par][0]);
+		
+		for(int kase = 0; kase < 10000; kase ++){
+			double delta = kase / 10000 * (Interval[par][1] - Interval[par][0]);	
+			double total = 1 / (Interval[par][1] - Interval[par][0]);
+			for(int k = 1; k < ttl; k ++){
+				tem = beta[par][k] * lambda[par] * exp( - pow((delta - x[par][k-1]), 2) / (2 * sigma[par] * sigma[par])  );
+				total = alpha[par][k] * (total + tem);
+			}
+			Output += total * LenIntervar;
+		}
+		
+		printf("%f\n", Output);
+	}
 
 
-
-	//Output and return
-	return 0;
+	return ;
 }
 
 
+/*===================DO NOT CHANGE ANYTHING ABOVE===================*/
 
 
-int main(){
+int main(int argc, char const *argv[]){
 /*	
 	//Function Instruction:
 	Main function, where is the begining of all program
@@ -169,16 +236,31 @@ int main(){
 
 	return 0;
 */
-
+	freopen("SaveArr", "w", stdout);
 	//Interval definition
 	double Interval[1][2];
-	Interval[0] = {0, 2}
+	Interval[0][0] = 0;
+	Interval[0][1] = 2;
 
 
-	Algorithm(1, 1000, 0.02, 0.3, );
+	Algorithm(1, 1000, 0.02, 0.3, Interval);
 	return 0;
 }
 
+
+double Cost(int var, double Parameter[var]){
+/*	
+	//Function Instruction:
+	This function will return cost in different situation, which you can change it.
+
+	//Parameter Instruction:
+	double cost valut;
+
+	return 0;
+*/	
+	double tem = pow(e, Parameter[0]) - 2;
+	return (tem > 0)? tem : -tem;
+}
 
 
 
